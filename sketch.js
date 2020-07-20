e45ea7377a42f6f4cb8c4266f8675aad15dd566a
MohakDhruv <67882815+MohakDhruv@users.noreply.github.com>
2020-07-20 14:17:02 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+
+function setup() {
+  createCanvas(800,400);
+ 
+}
+
+function draw() {
+  background("pink");  
+  drawSprites();
+ fill("chocolate")
+  rect(355,311, 110,55);
+  fill("tan");
+  rect(366,266,88,44);
+  fill("white");
+  rect(380,233,55,33)
+  fill("silver");
+  rect(311,366,220,110);
+fill("black")
+  textSize(55);
+text("Happy Birthday!", 255, 110,);
+}
